b7b98edc8e36fce665f268a0245658ee2dc01f6d
Bruno Cordeiro <brunocordeiro180@gmail.com>
2021-09-29 23:53:04 -0300

    Verifica erros semanticos de declaracao de variavel, main, e redeclaracao

---
+++ tests/teste.c
@@ -0,0 +1,21 @@
+// #include <stdio.h>
+// #include <stdlib.h>
+
+int teste(){
+    return 1;
+}
+
+int main()
+{
+    int a = 0;
+    int i;
+
+    for (i = 0; i < 10; i = i + 1)
+    {        
+        a = a + teste; 
+    }
+
+    x = 3 + 1;
+    int a;
+
+}
